7a8494b56003ed2afcfdc28a5875082afd07c38c
leo60228 <leo@60228.dev>
2026-06-07 17:39:45 -0400

    fix typo

--- src/pluralchum/webpackModules/main/utility.ts
+++ src/pluralchum/webpackModules/main/utility.ts
@@ -122,4 +122,4 @@ export async function sleep(timeout) {
   return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
-export const pluginName = 'Pluralchum';
+export const pluginName = 'pluralchum';
